21e4a31c464119630028cfa4d96d8da4d439c6ef
James Liu <james_liu@fastmail.com>
2018-10-20 19:55:53 +1100

    initial commit

---
+++ TimeToLeave/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TimeToLeave
+//
+//  Created by James Liu on 20/10/18.
+//  Copyright © 2018 James Liu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
