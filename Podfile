56fcdac1765aab177037f85c82c3dae07a77c9cb
James Liu <james_liu@fastmail.com>
2018-10-20 20:33:47 +1100

    feat: add alamofire/cocoapods, add menu bar icon

---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :osx, '10.14'
+use_frameworks!
+
+target 'TimeToLeave' do
+    pod 'Alamofire', '~> 4.7'
+end
--- TimeToLeave/AppDelegate.swift
+++ TimeToLeave/AppDelegate.swift
@@ -11,10 +11,21 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        if let button = statusItem.button {
+            button.image = NSImage(named: NSImage.Name("StatusBarButtonImage"))
+            button.action = #selector(printQuote(_:))
+        }
+    }
+    
+    @objc func printQuote(_ sender: Any?) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        print("\(quoteText) — \(quoteAuthor)")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
