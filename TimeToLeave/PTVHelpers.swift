7af4778eb180e115061f1f9e3f38dde358c8ec1a
James Liu <james_liu@fastmail.com>
2018-10-21 15:48:39 +1100

    feat: add Stop struct

--- TimeToLeave/PTVHelpers.swift
+++ TimeToLeave/PTVHelpers.swift
@@ -8,17 +8,28 @@
 import Foundation
 import SwiftPTV
 
+struct Stop {
+    var id: Int
+    var name: String
+    var routeTypes: [Int]
+}
+
 class PTVHelpers {
     static let communicator: SwiftPTV = SwiftPTV(
         apiKey: APICredentials.API_KEY,
         userID: APICredentials.API_DEVID)
     
     
-    static public func getStopsNear(location: Location, routeTypes: [Int], _ completionHandler: @escaping ([StopGeosearch]?) -> ()) {
+    static public func getStopsNear(location: Location, routeTypes: [Int], _ completionHandler: @escaping ([Stop]?) -> ()) {
         
         self.communicator.retrieveStopsNearLocation(location: Location(latitude: -37.8584388, longitude: 145.0268829), parameters: ["route_types": routeTypes.map { String($0) }, "max_distance": 1000]) { response in
             
-            completionHandler(response?.stops)
+            var stops: [Stop] = []
+            response?.stops?.forEach() {
+                stops.append(Stop(id: $0.ID!, name: $0.name!, routeTypes: [$0.routeType!]))
+            }
+            
+            completionHandler(stops)
         }
     }
 }
