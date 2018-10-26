2b6d47f01103dac1c4cdc0c88b4a462586fd48c8
James Liu <james_liu@fastmail.com>
2018-10-26 19:41:24 +1100

    feat: add Date extensions to convert to/from UTC time

---
+++ TimeToLeave/Utils.swift
@@ -0,0 +1,27 @@
+//
+//  Utils.swift
+//  TimeToLeave
+//
+//  Created by James Liu on 26/10/18.
+//  Copyright © 2018 James Liu. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    // Convert local time to UTC (or GMT)
+    func toGlobalTime() -> Date {
+        let timezone = TimeZone.current
+        let seconds = -TimeInterval(timezone.secondsFromGMT(for: self))
+        return Date(timeInterval: seconds, since: self)
+    }
+    
+    // Convert UTC (or GMT) to local time
+    func toLocalTime() -> Date {
+        let timezone = TimeZone.current
+        let seconds = TimeInterval(timezone.secondsFromGMT(for: self))
+        return Date(timeInterval: seconds, since: self)
+    }
+    
+}
